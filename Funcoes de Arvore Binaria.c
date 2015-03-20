8fe1595b27a0d472b820cdca69e15f944f7fe57e
thyago241 <thiagoribeironiquel@hotmail.com>
2015-03-20 14:52:41 -0300

    Update the name of compiler

--- Funcoes de Arvore Binaria.c
+++ Funcoes de Arvore Binaria.c
@@ -4,7 +4,7 @@ Codigo que recebe valores inteiros positivos a serem inseridos em uma arvore
 Este exibe a profundidade da arvore, a apresentacao dos valores dos nos,
 O numero de termos (nos) da arvore e a soma dos valore dos nos
 
-Compilador utilizado: gcc -g -o 2testArvore -W -Wall -pedantic -ansi -I. 2testArvore.c -lm
+Compilador utilizado: gcc -g -o nomeDoExecutavel -W -Wall -pedantic -ansi -I. nomeDoPrograma.c -lm
 */
 
 #include <stdio.h>
